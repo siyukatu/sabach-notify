4ea0f8698e36babc849f7b8f87d97ca437d143c9
siyukatu <120154870+siyukatu@users.noreply.github.com>
2024-08-10 22:11:47 +0900

    [バグ修正] 投票完了メッセージを認識できない & asyncioの処理がブロックされる

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 import time
 
 intents = discord.Intents.none()
@@ -7,16 +8,16 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-token = "<TOKEN>"
+token = "<token>"
 
 sabach_id = 1233072112139501608
 
 @client.event
 async def on_message(message):
-    if message.author.id == sabach_id and len(message.embeds) > 0 and message.embeds[0].footer == "voted":
+    if message.author.id == sabach_id and len(message.embeds) > 0 and message.embeds[0].footer != None and message.embeds[0].footer.text == "voted":
         next = message.embeds[0].fields[0].value.replace("<t:","").replace(":R>","")
         await message.channel.send(message.interaction_metadata.user.mention+" さんが投票しました。\n<t:"+next+":R> に通知します。")
-        time.sleep(int(next) - time.time())
+        await asyncio.sleep(int(next) - time.time())
         await message.channel.send(message.interaction_metadata.user.mention+" 投票が再度出来るようになりました。")
 
 client.run(token)
